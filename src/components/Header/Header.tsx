7217bd722f843b35f714862ff39c2be997e62d52
Yoo-Nah Park <ypark2270@gmail.com>
2025-05-24 17:54:00 -0400

    Add header with styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,14 @@
+import cn from 'classnames';
+import styles from './Header.module.css';
+
 interface HeaderProps {
   className?: string;
 }
 
 export const Header = ({ className }: HeaderProps) => {
-  return <div className={className}>Header</div>;
+  return (
+    <div className={cn(styles.container, className)}>
+      <h1 className={styles.tab}>iso-map</h1>
+    </div>
+  );
 };
