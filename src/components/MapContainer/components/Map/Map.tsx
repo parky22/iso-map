224a28224516b277988d8bc9d1802f1f15815d9e
Yoo-Nah Park <ypark2270@gmail.com>
2025-05-24 18:17:46 -0400

    Set up mapboxgl in Map

--- src/components/MapContainer/components/Map/Map.tsx
+++ src/components/MapContainer/components/Map/Map.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import styles from './Map.module.css';
+
 export const Map = () => {
-  return <div>Map</div>;
+  const mapRef = useRef<mapboxgl.Map | null>(null)
+  const mapRootRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapRootRef.current ?? 'map-root',
+    })
+
+    return () => {
+      mapRef.current?.remove();
+    }
+  }, [])
+
+  return <div id="map-root" ref={mapRootRef} className={styles.map} />;
 }; 
