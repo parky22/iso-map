8a5351a32147cedafce0b3b53c0f89caf98784f6
Yoo-Nah Park <ypark2270@gmail.com>
2025-05-25 19:23:32 -0400

    Wrap handleReset in useCallback

--- src/components/MapContainer/components/Map/Map.tsx
+++ src/components/MapContainer/components/Map/Map.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 
 import { INITIAL_CENTER, INITIAL_ZOOM } from './Map.constants';
 import { useMap } from './Map.hooks';
@@ -7,19 +8,19 @@ import styles from './Map.module.css';
 export const Map = () => {
   const { mapRef, mapRootRef, center, zoom } = useMap();
 
-  const handleOnClick = () => {
+  const handleReset = useCallback(() => {
     mapRef.current?.flyTo({
       center: INITIAL_CENTER,
       zoom: INITIAL_ZOOM,
     });
-  };
+  }, [mapRef]);
 
   return (
     <>
       <div className="sidebar">
         Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
       </div>
-      <button onClick={handleOnClick}>Reset</button>
+      <button onClick={handleReset}>Reset</button>
       <div id="map-root" ref={mapRootRef} className={styles.map} />
     </>
   );
