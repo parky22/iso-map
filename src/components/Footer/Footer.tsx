97bf10c41fee13accae5cd07f00a96091988b45d
Yoo-Nah Park <ypark2270@gmail.com>
2025-05-27 16:17:26 -0400

    Move github link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,12 @@ interface FooterProps {
   className?: string;
 }
 
+const PROJECT_GITHUB_URL = 'https://github.com/parky22/iso-map';
+
 export const Footer = ({ className }: FooterProps) => {
-  return <div className={className}>Footer</div>;
+  return (
+    <div className={className}>
+        <a href={PROJECT_GITHUB_URL} target="_blank" rel="noopener noreferrer">github</a>
+    </div>
+  );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,10 @@ interface HeaderProps {
   className?: string;
 }
 
-const PROJECT_GITHUB_URL = 'https://github.com/parky22/iso-map';
-
 export const Header = ({ className }: HeaderProps) => {
   return (
     <div className={cn(styles.container, className)}>
       <h1 className={styles.tab}>iso-map</h1>
-      <a href={PROJECT_GITHUB_URL} target="_blank" rel="noopener noreferrer">github link</a>
     </div>
   );
 };
