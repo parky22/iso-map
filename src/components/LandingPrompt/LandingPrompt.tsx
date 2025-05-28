e78bba36d6a26a4c9c37afd32c14686d32d59cdc
Yoo-Nah Park <ypark2270@gmail.com>
2025-05-28 17:46:56 -0400

    Add textValue state

--- src/components/LandingPrompt/LandingPrompt.tsx
+++ src/components/LandingPrompt/LandingPrompt.tsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
+
 export const LandingPrompt = () => {
+  const [textValue, setTextValue] = useState('')
+
+  const onTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextValue(e.target.value)
+  }
+
+  const onSubmit = () => {
+    console.log(textValue)
+  }
+
   return (
     <div className="landing-prompt">
       <h2>Where do you want to go?</h2>
       <div>
-        <textarea placeholder="Enter your destination" />
-        <button>Go</button>
+        <textarea
+          placeholder="Enter your destination"
+          value={textValue}
+          onChange={onTextChange}
+        />
+        <button onClick={onSubmit}>Go</button>
       </div>
     </div>
   );
