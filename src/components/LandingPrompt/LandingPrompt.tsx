0368d63f050a802cb90756e4b47c023e071610f9
Yoo-Nah Park <ypark2270@gmail.com>
2025-06-10 08:56:11 -0400

    Add styles to LandingPrompt

--- src/components/LandingPrompt/LandingPrompt.tsx
+++ src/components/LandingPrompt/LandingPrompt.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import styles from './LandingPrompt.module.css'
+
 export const LandingPrompt = () => {
   const [textValue, setTextValue] = useState('')
 
@@ -14,20 +16,19 @@ export const LandingPrompt = () => {
   }
 
   return (
-    <form className="landing-prompt" onSubmit={onSubmit}>
-      <label htmlFor="prompt">Where do you want to go?</label>
-      <div>
-        <textarea
-          id="prompt"
-          name="prompt"
-          rows={10}
-          cols={20}
-          placeholder="It's a beautiful day in the neighborhood..."
-          value={textValue}
-          onChange={onTextChange}
-        />
-        <button>Go</button>
-      </div>
+    <form className={styles.form} onSubmit={onSubmit}>
+      <label htmlFor="prompt" className={styles.label}>Where do you want to go?</label>
+      <textarea
+        className={styles.textarea}
+        id="prompt"
+        name="prompt"
+        rows={10}
+        cols={20}
+        placeholder="It's a beautiful day in the neighborhood..."
+        value={textValue}
+        onChange={onTextChange}
+      />
+      <button className={styles.button}>Go</button>
     </form>
   );
 };
